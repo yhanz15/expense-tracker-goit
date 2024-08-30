8233dd00601d160730ddb170747ff89ae13ffec7
yhanz15 <145173842+yhanz15@users.noreply.github.com>
2024-08-30 11:10:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { GlobalStyles } from 'styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/goit-expenses-tracker">
+  <BrowserRouter basename="/track-expenses">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
